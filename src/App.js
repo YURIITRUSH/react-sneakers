d952bf30775057ea809552ea9d444798febf6745
YURIITRUSH <yuriitrush47@gmail.com>
2022-07-17 14:16:57 +0300

    lesson 4

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
+import React from 'react'
 import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
 
-const arr = [
-  {name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl:'/img/sneakers/1.jpg'},
-  {name: 'Мужские Кроссовки Nike Air Max 270', price: 15600, imageUrl:'/img/sneakers/2.jpg'},
-  {name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 8499, imageUrl:'/img/sneakers/3.jpg'},
-  {name: 'Кроссовки Puma X Aka Boku Future Rider', price: 8999, imageUrl:'/img/sneakers/4.jpg'},
-]
 
 function App() {
+  const [items, setItems] = React.useState([]);
+  const [cardItems, setCardItems] = React.useState([]);
+  const [cardOpened, setCardOpended] = React.useState(false);
+
+  React.useEffect(()=>{
+    fetch('https://62d2f56181cb1ecafa688459.mockapi.io/items')
+    .then((res) => {
+      return res.json();
+    }).then((json) => {
+      setItems(json);
+    });
+  }, []);
+
+  const onAddToCard = (obj) => {
+    setCardItems((prev) => [...prev, obj]);
+  }
+  
   return (
     <div className="wrapper clear">
-      <Drawer />
-      <Header />
+      {cardOpened && <Drawer items={cardItems} onClose={() => {setCardOpended(false)}} />}
+      <Header onClickCard={() => {setCardOpended(true)}} />
       <div className="conent p-40">
         <div className="d-flex align-center justify-between mb-40">
           <h1>Все кроссовки</h1>
@@ -23,9 +35,15 @@ function App() {
           </div>
         </div>
 
-        <div className="d-flex">
-          {arr.map((obj) => (
-            <Card title={obj.name} price={obj.price} imageUrl={obj.imageUrl} onClick={() => console.log(obj)} />
+        <div className="d-flex flex-wrap">
+          {items.map((item) => (
+            <Card
+              title={item.title}
+              price={item.price}
+              imageUrl={item.imageUrl}
+              onFavorite={() => console.log('Добавили в закладки')}
+              onPlus={(obj) => onAddToCard(obj)}
+            />
           ))}
         </div>
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-function Header() {
+function Header(props) {
   return (
     <header className="d-flex justify-between align-center p-40">
       <div className="d-flex align-center">
@@ -12,7 +12,7 @@ function Header() {
       </div>
       <div>
         <ul className="d-flex align-center">
-          <li className="mr-30">
+          <li onClick={props.onClickCard} className="mr-30  cu-p">
             <img src="/img/cart.svg" alt="cart" />
             <span>
               1205 руб.
